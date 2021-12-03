b4b5f1cbb35ca1bda0e3a0f3b425e1376fd86d38
Jasper Hendriks <j.hendriks@sigmax.nl>
2021-12-03 09:07:53 +0100

    Day 3 assignment. to much duplicated code.

---
+++ AdventOfCode2021/Assignments/Day3/InputHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace AdventOfCode2021.Assignments.Day3
+{
+    public static class InputHandler
+    {
+        public static IList<string> GetInputAsList(string input)
+        {
+            var lines = input.Split(new string[] { Environment.NewLine },
+                    StringSplitOptions.None);
+
+            var result = lines.ToList();
+            return result;
+        }
+
+    }
+
+}
