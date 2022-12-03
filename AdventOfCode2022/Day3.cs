995b8fd7c2aedf7666a307558c8d8f66ea43b51b
Jasper Hendriks <j.hendriks@sigmax.nl>
2022-12-03 09:43:48 +0100

    Day 3

---
+++ AdventOfCode2022/Day3.cs
@@ -0,0 +1,72 @@
+﻿namespace AdventOfCode2022.Assignments
+{
+    /**
+     * 
+     */
+    public class Day3 : IDay
+    {
+        public int PartA(string input)
+        {
+            var inputData = ProcessInput(input);
+            var result = 0;
+            foreach(var line in inputData)
+            {
+                var c = FindDuplicateItem(line);
+                result += ConvertToPrio(c);
+            }
+         
+            return result;
+        }
+
+        public int PartB(string input)
+        {
+            var inputData = ProcessInput(input);
+            var result = 0;
+
+            // sets of 3
+            for (int i = 0; i <= inputData.Count - 3; i += 3)
+            {
+
+                var c = FindDuplicateItem(inputData[i], inputData[i + 1], inputData[i + 2]);
+                result += ConvertToPrio(c);
+            }
+
+            return result;
+        }
+
+        public static char FindDuplicateItem(string v1, string v2, string v3)
+        {
+            var intersection = v1.Intersect(v2).Intersect(v3);
+            return intersection.Single();
+        }
+
+        public static IList<string> ProcessInput(string input)
+        {
+            var lines = input.Split(new string[] { Environment.NewLine},
+                    StringSplitOptions.None);
+            
+            return lines;
+        }
+
+        public static int ConvertToPrio(char asciiVal)
+        {
+            if (asciiVal < 97)
+            {
+                return asciiVal - 38; // Offset to 27
+            }
+
+            return asciiVal - 96; // Offset to 1
+        }
+
+        public static char FindDuplicateItem(string input)
+        {
+            string firsthalf = input.Substring(0, input.Length / 2);
+            string secondhalf = input.Substring(input.Length / 2);
+
+
+            var intersection = firsthalf.Intersect(secondhalf);
+            return intersection.Single();
+        }
+    }
+
+}
---
+++ AdventOfCode2022Tests/Day3Test.cs
@@ -0,0 +1,75 @@
+namespace AdventOfCode2022Tests
+{
+    [TestClass]
+    public class Day3Test
+    {
+        private const int AnswerPartA = 7889;
+        private const int AnswerPartB = 2825;
+        private const int AnswerExamplePartA = 157;
+        private const int AnswerExamplePartB = 70;
+
+
+        private readonly IDay day = new Day3();
+        private readonly IDayInput input = new Day3Input();
+
+        [TestMethod]
+        public void PartAExample()
+        {
+            var result = day.PartA(input.ExampleInput);
+            Assert.AreEqual(AnswerExamplePartA, result);
+        }
+
+        [TestMethod]
+        public void PartA()
+        {
+            var result = day.PartA(input.Input);
+            Assert.AreEqual(AnswerPartA, result);
+        }
+
+        [TestMethod]
+        public void PartBExample()
+        {
+            var result = day.PartB(input.ExampleInput);
+            Assert.AreEqual(AnswerExamplePartB, result);
+        }
+
+        [TestMethod]
+        public void PartB()
+        {
+            var result = day.PartB(input.Input);
+            Assert.AreEqual(AnswerPartB, result);
+        }
+
+        [TestMethod]
+        public void ConvertTestLower()
+        {
+            Assert.AreEqual(1, Day3.ConvertToPrio('a'));
+        }
+
+        [TestMethod]
+        public void ConvertTestUpper()
+        {
+            Assert.AreEqual(27, Day3.ConvertToPrio('A'));
+        }
+
+        [TestMethod]
+        public void TestDuplicateFinder()
+        {
+            Assert.AreEqual('A', Day3.FindDuplicateItem("ABCA"));
+            Assert.AreEqual('A', Day3.FindDuplicateItem("BAAC"));
+            Assert.ThrowsException<InvalidOperationException>(() => Day3.FindDuplicateItem("BAaC"));
+        }
+
+        [TestMethod]
+        public void TestDuplicateFinderV2()
+        {
+            // Examples
+            Assert.AreEqual('r', Day3.FindDuplicateItem("vJrwpWtwJgWrhcsFMMfFFhFp", "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "PmmdzqPrVvPwwTWBwg"));
+            Assert.AreEqual('Z', Day3.FindDuplicateItem("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", "ttgJtRGJQctTZtZT", "CrZsJsPPZsGzwwsLwLmpwMDw"));
+            
+            Assert.AreEqual('a', Day3.FindDuplicateItem("aBCD", "ABCDa", "abcde"));
+            Assert.ThrowsException<InvalidOperationException>(() => Day3.FindDuplicateItem("ABCD", "ABCD", "ABCD"));
+
+        }
+    }
+}
