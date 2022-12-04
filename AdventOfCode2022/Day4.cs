d1b6292b51f43ce1834169580d141a5c23adda7d
Jasper Hendriks <j.hendriks@sigmax.nl>
2022-12-04 09:23:58 +0100

    Day 4

---
+++ AdventOfCode2022/Day4.cs
@@ -0,0 +1,88 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AdventOfCode2022.Assignments
+{
+    /**
+     * 
+     */
+    public class Day4 : IDay
+    {
+        public int PartA(string input)
+        {
+            var inputData = ProcessInput(input);
+            var containsCount = 0;
+            for (int i = 0; i < inputData.Count; i++)
+            {
+                if (Contains(inputData[i].Item1, inputData[i].Item2))
+                {
+                    containsCount++;
+                }
+            }
+            return containsCount;
+        }
+
+        public int PartB(string input)
+        {
+            var inputData = ProcessInput(input);
+            var overlapsCount = 0;
+            for (int i = 0; i < inputData.Count; i++)
+            {
+                if (Overlaps(inputData[i].Item1, inputData[i].Item2))
+                {
+                    overlapsCount++;
+                }
+            }
+
+            return overlapsCount;
+        }
+
+        public static IList<Tuple<Range, Range>> ProcessInput(string input)
+        {
+            var lines = input.Split(new string[] { Environment.NewLine },
+                    StringSplitOptions.None);
+
+            var result = new List<Tuple<Range, Range>>();
+            foreach (var line in lines)
+            {
+                var ranges = line.Split(',', '-').Select(item => int.Parse(item)).ToList();
+                var r1 = new Range(ranges[0], ranges[1]);
+                var r2 = new Range(ranges[2], ranges[3]);
+                result.Add(new Tuple<Range, Range>(r1, r2));
+            }
+
+            return result;
+        }
+
+        public static bool Contains(Range left, Range right)
+        {
+            if (left.Start.Value <= right.Start.Value && left.End.Value >= right.End.Value ||
+                right.Start.Value <= left.Start.Value && right.End.Value >= left.End.Value)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool Overlaps(Range left, Range right)
+        {
+            var leftStart = left.Start.Value;
+            var leftEnd = left.End.Value;
+            var rightStart = right.Start.Value;
+            var rightEnd = right.End.Value;
+
+            if (leftStart >= rightStart && leftStart <= rightEnd ||
+                rightStart >= leftStart && rightStart <= leftEnd ||
+                leftEnd >= rightStart && leftEnd <= rightEnd ||
+                rightEnd >= leftStart && rightEnd <= leftEnd)
+            {
+                return true;
+            }
+
+
+            return false;
+        }
+
+    }
+
+}
---
+++ AdventOfCode2022Tests/Day4Test.cs
@@ -0,0 +1,66 @@
+namespace AdventOfCode2022Tests
+{
+    [TestClass]
+    public class Day4Test
+    {
+        private const int AnswerPartA = 588;
+        private const int AnswerPartB = 911;
+        private const int AnswerExamplePartA = 2;
+        private const int AnswerExamplePartB = 4;
+
+
+        private readonly IDay day = new Day4();
+        private readonly IDayInput input = new Day4Input();
+
+        [TestMethod]
+        public void PartAExample()
+        {
+            var result = day.PartA(input.ExampleInput);
+            Assert.AreEqual(AnswerExamplePartA, result);
+        }
+
+        [TestMethod]
+        public void PartA()
+        {
+            var result = day.PartA(input.Input);
+            Assert.AreEqual(AnswerPartA, result);
+        }
+
+        [TestMethod]
+        public void PartBExample()
+        {
+            var result = day.PartB(input.ExampleInput);
+            Assert.AreEqual(AnswerExamplePartB, result);
+        }
+
+        [TestMethod]
+        public void PartB()
+        {
+            var result = day.PartB(input.Input);
+            Assert.AreEqual(AnswerPartB, result);
+        }
+
+        [TestMethod]
+        public void ContainsTest()
+        {
+            Assert.AreEqual(true, Day4.Contains(new Range(0, 4), new Range(1, 4)));
+            Assert.AreEqual(true, Day4.Contains(new Range(1, 4), new Range(1, 4)));
+            Assert.AreEqual(true, Day4.Contains(new Range(2, 4), new Range(1, 4)));
+            Assert.AreEqual(true, Day4.Contains(new Range(3, 3), new Range(1, 4)));
+            Assert.AreEqual(false, Day4.Contains(new Range(5, 9), new Range(1, 4)));
+            Assert.AreEqual(false, Day4.Contains(new Range(1, 4), new Range(5, 9)));
+        }
+
+        [TestMethod]
+        public void OverlapTest()
+        {
+            Assert.AreEqual(true, Day4.Overlaps(new Range(0, 4), new Range(1, 5)));
+            Assert.AreEqual(true, Day4.Overlaps(new Range(1, 4), new Range(1, 4)));
+            Assert.AreEqual(true, Day4.Overlaps(new Range(2, 4), new Range(1, 4)));
+            Assert.AreEqual(true, Day4.Overlaps(new Range(3, 3), new Range(1, 4)));
+            Assert.AreEqual(false, Day4.Overlaps(new Range(1, 3), new Range(5, 8)));
+            Assert.AreEqual(false, Day4.Overlaps(new Range(5, 8), new Range(1, 3)));
+
+        }
+    }
+}
