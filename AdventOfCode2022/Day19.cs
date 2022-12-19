e0a7ae5d5aed8e32f36e2c166bd3fa2ef257bd4a
Jasper Hendriks <j.hendriks@sigmax.nl>
2022-12-19 23:05:43 +0100

    day 19 cleanup

--- AdventOfCode2022/Day19.cs
+++ AdventOfCode2022/Day19.cs
@@ -9,25 +9,12 @@ namespace AdventOfCode2022.Assignments
     {
         public int Part1(string input)
         {
-            var blueprints = createBlueprint(input);
+            var blueprints = ProcessInput(input);
             int qualities = 0;
 
             for (int i = 0; i < blueprints.Count; i++)
             {
-                int ore = 0;
-                int oreBots = 1;
-                int clay = 0;
-                int clayBots = 0;
-                int obsidian = 0;
-                int obsidianBots = 0;
-                int geode = 0;
-                int geodeBots = 0;
-
-                int round = 0;
-
-                var blueprint = blueprints[i];
-
-                int newScore = doBlueprint(blueprint, ore, oreBots, clay, clayBots, obsidian, obsidianBots, geode, geodeBots, round, 24);
+                int newScore = ProcessBlueprint(blueprint: blueprints[i], ore: 0, oreBots: 1, clay: 0, clayBots: 0, obsidian: 0, obsidianBots: 0, geode: 0, geodeBots: 0, minutesLeft: 24);
                 qualities += newScore * (i + 1);
             }
 
@@ -36,34 +23,15 @@ namespace AdventOfCode2022.Assignments
 
         public int Part2(string input)
         {
-            int maxOre;
-            int maxClay;
-            int maxObs;
-            var blueprints = createBlueprint(input);
-            var qualities = 1;
+            var blueprints = ProcessInput(input);
+            var score = 1;
             for (int i = 0; i < Math.Min(3, blueprints.Count); i++)
             {
-                int ore = 0;
-                int oreBots = 1;
-                int clay = 0;
-                int clayBots = 0;
-                int obsidian = 0;
-                int obsidianBots = 0;
-                int geode = 0;
-                int geodeBots = 0;
-
-                int round = 0;
-
-                var blueprint = blueprints[i];
-
-                
-
-                int newScore = doBlueprint(blueprint, ore, oreBots, clay, clayBots, obsidian, obsidianBots, geode, geodeBots, round, 32);
-                qualities *= newScore;
+                int newScore = ProcessBlueprint(blueprint: blueprints[i], ore: 0, oreBots: 1, clay: 0, clayBots: 0, obsidian: 0, obsidianBots: 0, geode: 0, geodeBots: 0, minutesLeft: 32);
+                score *= newScore;
             }
             
-
-            return qualities;
+            return score;
         }
         public struct Blueprint
         {
@@ -71,19 +39,19 @@ namespace AdventOfCode2022.Assignments
             public int ObsidianOre, ObsidianClay;
             public int GeodeOre, GeodeObsidian;
 
-            public Blueprint(int ore, int clay, int oOre, int oClay, int geOre, int geObsidian)
+            public Blueprint(int[] input)
             {
-                Ore = ore;
-                Clay = clay;
-                ObsidianOre = oOre;
-                ObsidianClay = oClay;
-                GeodeOre = geOre;
-                GeodeObsidian = geObsidian;
+                Ore = input[0];
+                Clay = input[1];
+                ObsidianOre = input[2];
+                ObsidianClay = input[3];
+                GeodeOre = input[4];
+                GeodeObsidian = input[5];
             }
         }
 
   
-        public static int doBlueprint(
+        public static int ProcessBlueprint(
                 Blueprint blueprint,
                 int ore,
                 int oreBots,
@@ -93,11 +61,8 @@ namespace AdventOfCode2022.Assignments
                 int obsidianBots,
                 int geode,
                 int geodeBots,
-                int round,
-                int maxRounds)
+                int minutesLeft)
         {
-            int maxScore = 0;
-            int bestFrom = 0;
             bool prevCanOre = false;
             bool prevCanClay = false;
             bool prevCanObs = false;
@@ -107,7 +72,7 @@ namespace AdventOfCode2022.Assignments
             var maxClay = blueprint.ObsidianClay;
             var maxObs = blueprint.GeodeObsidian;
 
-            for (int i = round; i < maxRounds; i++)
+            for (int i = minutesLeft - 1; i >= 0; i--)
             {
                 bool canOre = blueprint.Ore <= ore;
                 bool canClay = blueprint.Clay <= ore;
@@ -121,25 +86,25 @@ namespace AdventOfCode2022.Assignments
                 int subscore;
                 if (canGeo && !prevCanGeo)
                 {
-                    subscore = doBlueprint(blueprint, ore - blueprint.GeodeOre, oreBots, clay, clayBots, obsidian - blueprint.GeodeObsidian, obsidianBots, geode, geodeBots + 1, i + 1, maxRounds);
+                    subscore = ProcessBlueprint(blueprint, ore - blueprint.GeodeOre, oreBots, clay, clayBots, obsidian - blueprint.GeodeObsidian, obsidianBots, geode, geodeBots + 1, i);
                     score = Math.Max(score, subscore);
                     prevCanGeo = canGeo;
                 }
                 if (canObs && !prevCanObs && obsidianBots < maxObs)
                 {
-                    subscore = doBlueprint(blueprint, ore - blueprint.ObsidianOre, oreBots, clay - blueprint.ObsidianClay, clayBots, obsidian, obsidianBots + 1, geode, geodeBots, i + 1, maxRounds);
+                    subscore = ProcessBlueprint(blueprint, ore - blueprint.ObsidianOre, oreBots, clay - blueprint.ObsidianClay, clayBots, obsidian, obsidianBots + 1, geode, geodeBots, i);
                     score = Math.Max(score, subscore);
                     prevCanObs = canObs;
                 }
                 if (canClay && !prevCanClay && clayBots < maxClay)
                 {
-                    subscore = doBlueprint(blueprint, ore - blueprint.Clay, oreBots, clay, clayBots + 1, obsidian, obsidianBots, geode, geodeBots, i + 1, maxRounds);
+                    subscore = ProcessBlueprint(blueprint, ore - blueprint.Clay, oreBots, clay, clayBots + 1, obsidian, obsidianBots, geode, geodeBots, i);
                     score = Math.Max(score, subscore);
                     prevCanClay = canClay;
                 }
                 if (canOre && !prevCanOre && oreBots < maxOre)
                 {
-                    subscore = doBlueprint(blueprint, ore - blueprint.Ore, oreBots + 1, clay, clayBots, obsidian, obsidianBots, geode, geodeBots, i + 1, maxRounds);
+                    subscore = ProcessBlueprint(blueprint, ore - blueprint.Ore, oreBots + 1, clay, clayBots, obsidian, obsidianBots, geode, geodeBots, i);
                     score = Math.Max(score, subscore);
                     prevCanOre = canOre;
                 }
@@ -149,32 +114,23 @@ namespace AdventOfCode2022.Assignments
             return score;
         }
 
-        private static List<Blueprint> createBlueprint(string input)
+        private static List<Blueprint> ProcessInput(string input)
         {
             var blueprints = new List<Blueprint>();
 
-            var processed = ProcessInput(input);
-            foreach (var blueprint in processed)
-            {
-                blueprints.Add(new Blueprint(blueprint[1], blueprint[2], blueprint[3], blueprint[4], blueprint[5], blueprint[6]));
-
-            }
-            return blueprints;
-        }
-
-        public static List<int[]> ProcessInput(string input)
-        {
             var lines = input.Split(new string[] { Environment.NewLine },
                     StringSplitOptions.None);
             var result = new List<int[]>();
             foreach (var line in lines)
             {
                 var numbers = Regex.Split(line, @"\D+");
-                var n1 = numbers.Where(n => !string.IsNullOrEmpty(n)).Select(n => int.Parse(n)).ToArray();
-                result.Add(n1);
+                var blueprintInput = numbers.Where(n => !string.IsNullOrEmpty(n)).Skip(1).Select(n => int.Parse(n)).ToArray();
+                blueprints.Add(new Blueprint(blueprintInput));
+
             }
 
-            return result;
+            return blueprints;
         }
+
     }
 }
