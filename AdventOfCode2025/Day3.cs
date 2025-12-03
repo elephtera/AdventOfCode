8449f29b92f5f6f8f4d00488b53a1d3746aeb6f2
Jasper Hendriks <elephtera@outlook.com>
2025-12-03 07:56:50 +0100

    day 3, part A

---
+++ AdventOfCode2025/Day3.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq.Expressions;
+
+namespace AdventOfCode2025
+{
+    /**
+     * 
+     */
+    public class Day3 : IDay<long>
+    {
+        public long Part1(string input)
+        {
+            var inputData = ProcessInput(input);
+            var result = 0;
+
+
+            foreach (var row in inputData)
+            {
+                result += CalcJolt(row);
+            }
+
+            return result;
+        }
+
+        private int CalcJolt(int[] row)
+        {
+            var maxJolt = 0;
+            for (var i = 0; i < row.Length - 1; i++)
+            {
+                var firstDigit = row[i] * 10;
+                foreach(var secondDigit in row.Skip(i+1))
+                {
+                    var jolt = firstDigit + secondDigit;
+                    if (jolt > maxJolt)
+                    {
+                        maxJolt = jolt;
+                    }
+                }
+            }
+            return maxJolt;
+        }
+
+        public long Part2(string input)
+        {
+            var inputData = ProcessInput(input);
+            var result = 0;
+            return result;
+        }
+
+        public static IList<int[]> ProcessInput(string input)
+        {
+            var lines = input.Split(new string[] { Environment.NewLine},
+                    StringSplitOptions.None);
+            var res = new List<int[]>();
+            foreach (var line in lines)
+            {
+                res.Add(Array.ConvertAll(line.ToCharArray(), c => int.Parse(c.ToString())));
+            }
+
+            return res;
+        }
+    }
+
+}
---
+++ AdventOfCode2025Tests/Day3Test.cs
@@ -0,0 +1,46 @@
+using AdventOfCode2025;
+using AdventOfCode2025Tests.Input;
+
+namespace AdventOfCode2025Tests
+{
+    [TestClass]
+    public class Day3Test
+    {
+        private const long AnswerPart1 = 0;
+        private const long AnswerPart2 = 0;
+        private const long AnswerExamplePart1 = 357;
+        private const long AnswerExamplePart2 = 0;
+
+
+        private readonly IDay<long> day = new Day3();
+        private readonly IDayInput input = new Day3Input();
+
+        [TestMethod]
+        public void Part1Example()
+        {
+            var result = day.Part1(input.ExampleInput);
+            Assert.AreEqual(AnswerExamplePart1, result);
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            var result = day.Part1(input.Input);
+            Assert.AreEqual(AnswerPart1, result);
+        }
+
+        [TestMethod]
+        public void Part2Example()
+        {
+            var result = day.Part2(input.ExampleInput);
+            Assert.AreEqual(AnswerExamplePart2, result);
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            var result = day.Part2(input.Input);
+            Assert.AreEqual(AnswerPart2, result);
+        }
+    }
+}
