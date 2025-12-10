cdb5efcd2d9b95e04a3685e233bdf162aa202b9a
Jasper Hendriks <jasper.hendriks@vormer.it>
2025-12-10 15:49:57 +0100

    Day 10A

---
+++ AdventOfCode2025/Day10.cs
@@ -0,0 +1,131 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2025
+{
+    /**
+     * 
+     */
+    public class Day10 : IDay<long>
+    {
+        public long Part1(string input)
+        {
+            IList<(uint lights, IList<uint> bitmasks, IList<int> jolts)> inputData = ProcessInput(input);
+
+            var result = 0;
+
+            foreach (var (lights, bitmasks, jolts) in inputData)
+            {
+                result += CalculateToggles(lights, bitmasks, 0, 0);
+            }
+            return result;
+        }
+
+        public static int CalculateToggles(uint lights, IList<uint> bitmasks, uint currentLightsOn, int position)
+        {
+
+            // try to get the "lights" value as result for currentLightsOn by applying the bitmasks in any combination
+
+
+            // check if currentLightsOn is equal to lights, after applying the next bitmask
+
+            if (currentLightsOn == lights)
+            {
+                return 0;
+            }
+            if (position >= bitmasks.Count)
+            {
+                return int.MaxValue;
+            }
+
+            var val1 = CalculateToggles(lights, bitmasks, currentLightsOn ^ bitmasks[position], position + 1);
+            var val2 = CalculateToggles(lights, bitmasks, currentLightsOn, position + 1);
+            return Math.Min(val1 == int.MaxValue ? int.MaxValue : val1 + 1, val2);
+
+        }
+
+        public long Part2(string input)
+        {
+            var inputData = ProcessInput(input);
+            var result = 0;
+            return result;
+        }
+
+        public static IList<(uint lights, IList<uint> bitmasks, IList<int> jolts)> ProcessInput(string input)
+        {
+            var lines = input.Split(new string[] { Environment.NewLine },
+                    StringSplitOptions.None);
+
+            var result = new List<(uint lights, IList<uint> bitmasks, IList<int> jolts)>();
+
+           
+            var bracketRegex = new Regex(@"\[.*\]");
+            var bitmaskRegex = new Regex(@"\(.*?\)");
+            var joltsRegex = new Regex(@"\{.*\}");
+            foreach (var line in lines)
+            {
+                var lights = 0u;
+                var bitmasks = new List<uint>();
+                //regex all between brackets, and convert the string with dots and hashtags to zero's and ones as bitwise representation of an int
+                var match = bracketRegex.Match(line);
+                var lightsLength = 0;
+                if (match.Success)
+                {
+                    var inner = match.Groups[0].Value[1..^1];
+                    lights = CalculateLightEndstate(inner);
+                    lightsLength = inner.Length;
+                }
+
+                var match2 = bitmaskRegex.Matches(line);
+                if (match2.Count > 0)
+                {
+                    foreach (var group in match2)
+                    {
+                        uint bitmask = CalculateLightswitch(lightsLength, group.ToString()[1..^1]);
+                        bitmasks.Add(bitmask);
+                    }
+                }
+
+                match = joltsRegex.Match(line);
+                var jolts = new List<int>();
+                if (match.Success)
+                {
+                    var inner = match.Groups[0].Value[1..^1].Split(',');
+
+                    for (int i = 0; i < inner.Length; i++)
+                    {
+                        jolts.Add(int.Parse(inner[i]));
+                    }
+
+                }
+                result.Add((lights, bitmasks, jolts));
+            }
+            return result;
+        }
+
+        public static uint CalculateLightEndstate(string lightEndstate)
+        {
+            var bitmask = Convert.ToUInt32(lightEndstate.Replace(".", "0").Replace("#", "1"), 2);
+            return bitmask;
+        }
+
+        public static uint CalculateLightswitch(int lightsLength, string switchData)
+        {
+            var inner = switchData.Split(',');
+            char[] mask = new char[lightsLength];
+            for (int j = 0; j < mask.Length; j++)
+            {
+                mask[j] = '0';
+            }
+
+            for (int i = 0; i < inner.Length; i++)
+            {
+                int maskIndex = int.Parse(inner[i]);
+                mask[maskIndex] = '1';
+            }
+
+            var bitmask = Convert.ToUInt32(new string(mask), 2);
+            return bitmask;
+        }
+    }
+
+}
---
+++ AdventOfCode2025Tests/Day10Test.cs
@@ -0,0 +1,186 @@
+using AdventOfCode2025;
+using AdventOfCode2025Tests.Input;
+
+namespace AdventOfCode2025Tests
+{
+    [TestClass]
+    public class Day10Test
+    {
+        private const long AnswerPart1 = 432;
+        private const long AnswerPart2 = 0;
+        private const long AnswerExamplePart1 = 7;
+        private const long AnswerExamplePart2 = 0;
+
+
+        private readonly IDay<long> day = new Day10();
+        private readonly IDayInput input = new Day10Input();
+
+        [TestMethod]
+        public void Part1Example()
+        {
+            var result = day.Part1(input.ExampleInput);
+            Assert.AreEqual(AnswerExamplePart1, result);
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            var result = day.Part1(input.Input);
+            Assert.AreEqual(AnswerPart1, result);
+        }
+
+        [TestMethod]
+        public void Part2Example()
+        {
+            var result = day.Part2(input.ExampleInput);
+            Assert.AreEqual(AnswerExamplePart2, result);
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            var result = day.Part2(input.Input);
+            Assert.AreEqual(AnswerPart2, result);
+        }
+
+        [TestMethod]
+        public void LightSwitchTest()
+        {
+            var result = Day10.CalculateLightswitch(4, "1,2"); // 0b0110
+            Assert.AreEqual(6u, result);
+
+            result = Day10.CalculateLightswitch(4, "0"); // 0b0001
+            Assert.AreEqual(1u, result);
+        }
+
+        [TestMethod]
+        public void LightEndstateTest()
+        {
+            var result = Day10.CalculateLightEndstate("..#."); // 0b0010
+            Assert.AreEqual(2u, result);
+
+            result = Day10.CalculateLightEndstate("...#"); // 0b0010
+            Assert.AreEqual(1u, result);
+        }
+
+        [TestMethod]
+        public void CalcToggleTest()
+        {
+            var result = Day10.CalculateToggles(
+                Day10.CalculateLightEndstate("..#."),
+                new List<uint> {
+                    Day10.CalculateLightswitch(4, "2"),
+                    Day10.CalculateLightswitch(4, "1")
+                               },
+                0,
+                0);
+            Assert.AreEqual(1, result);
+
+            result = Day10.CalculateToggles(
+                Day10.CalculateLightEndstate(".##."),
+                new List<uint> {
+                    Day10.CalculateLightswitch(4, "0,2"),
+                    Day10.CalculateLightswitch(4, "0,1")
+                               },
+                0,
+                0);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void CalcToggleTestExample1()
+        {
+            // [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1)
+            var result = Day10.CalculateToggles(
+                Day10.CalculateLightEndstate(".##."),
+                new List<uint> {
+                    Day10.CalculateLightswitch(4, "3"),
+                    Day10.CalculateLightswitch(4, "1,3"),
+                    Day10.CalculateLightswitch(4, "2"),
+                    Day10.CalculateLightswitch(4, "2,3"),
+                    Day10.CalculateLightswitch(4, "0,2"),
+                    Day10.CalculateLightswitch(4, "0,1")
+                               },
+                0,
+                0);
+            Assert.AreEqual(2, result);
+
+            var input = Day10.ProcessInput("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}");
+            Assert.AreEqual(1, input.Count);
+            Assert.AreEqual(0b0110u, input[0].lights);
+            Assert.AreEqual(6, input[0].bitmasks.Count);
+            Assert.AreEqual(4, input[0].jolts.Count);
+
+
+
+            var result2 = day.Part1("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}");
+            Assert.AreEqual(2, result2);
+        }
+
+        [TestMethod]
+        public void Part1SeperateExampleTest()
+        {
+            var result = day.Part1("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}");
+            Assert.AreEqual(2, result);
+
+            result = day.Part1("[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}");
+            Assert.AreEqual(3, result);
+
+            result = day.Part1("[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}");
+            Assert.AreEqual(2, result);
+
+
+            result = day.Part1(@"[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}");
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void CalcToggleTestExample2()
+        {
+            // [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+
+            var input = Day10.ProcessInput("[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}");
+            Assert.AreEqual(1, input.Count);
+            Assert.AreEqual(0b00010u, input[0].lights);
+            Assert.AreEqual(5, input[0].bitmasks.Count);
+            Assert.AreEqual(5, input[0].jolts.Count);
+
+            Assert.AreEqual(0b10111u, input[0].bitmasks[0]);
+            Assert.AreEqual(0b00110u, input[0].bitmasks[1]);
+            Assert.AreEqual(0b10001u, input[0].bitmasks[2]);
+            Assert.AreEqual(0b11100u, input[0].bitmasks[3]);
+            Assert.AreEqual(0b01111u, input[0].bitmasks[4]);
+
+
+            var result = Day10.CalculateToggles(
+                Day10.CalculateLightEndstate("...#."),
+                new List<uint> {
+                    Day10.CalculateLightswitch(5, "0,4"),
+                    Day10.CalculateLightswitch(5, "0,1,2"),
+                    Day10.CalculateLightswitch(5, "1,2,3,4")
+                               },
+                0,
+                0);
+            Assert.AreEqual(3, result);
+
+            result = Day10.CalculateToggles(
+                Day10.CalculateLightEndstate("...#."),
+                new List<uint> {
+                    Day10.CalculateLightswitch(5, "0,2,3,4"),
+                    Day10.CalculateLightswitch(5, "2,3"),
+                    Day10.CalculateLightswitch(5, "0,4"),
+                    Day10.CalculateLightswitch(5, "0,1,2"),
+                    Day10.CalculateLightswitch(5, "1,2,3,4")
+                               },
+                0,
+                0);
+            Assert.AreEqual(3, result);
+
+
+
+            var result2 = day.Part1("[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}");
+            Assert.AreEqual(3, result2);
+        }
+    }
+}
