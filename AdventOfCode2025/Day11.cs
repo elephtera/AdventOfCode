0fb6278e6432f9abc7f06dd0d8e28458a099fde2
Jasper Hendriks <jasper.hendriks@vormer.it>
2025-12-11 10:48:59 +0100

    Day 11

---
+++ AdventOfCode2025/Day11.cs
@@ -0,0 +1,132 @@
+﻿namespace AdventOfCode2025
+{
+    /**
+     * 
+     */
+    public class Day11 : IDay<long>
+    {
+        public long Part1(string input)
+        {
+            IList<(string id, IList<string> outputs)> inputData = ProcessInput(input);
+
+            var result = 0L;
+
+            var me = inputData.Where(item => item.id == "you").First();
+
+            foreach (var output in me.outputs)
+            {
+                result += ProcessNextItem(output, inputData);
+                // Process each output
+            }
+
+
+            return result;
+        }
+
+        private long ProcessNextItem(string input, IList<(string id, IList<string> outputs)> inputData)
+        {
+            if (Memo.ContainsKey(input))
+            {
+                return Memo[input];
+            }
+            var result = 0L;
+            var outputs = inputData.Where(item => item.id == input).Single().outputs;
+            foreach (var output in outputs)
+            {
+                if (output == "out")
+                {
+                    result += 1;
+                    continue;
+                }
+
+                result += ProcessNextItem(output, inputData);
+                // Process each output
+            }
+
+            Memo.Add(input, result);
+            return result;
+        }
+
+        private long ProcessNextItem3(string node, IList<(string id, IList<string> outputs)> inputData, string endNode, string other)
+        {
+            if (Memo.ContainsKey(node))
+            {
+                return Memo[node];
+            }
+            var result = 0L;
+            var outputs = inputData.Where(item => item.id == node).Single().outputs;
+            foreach (var output in outputs)
+            {
+                if (output == endNode)
+                {
+                    result += 1;
+                    continue;
+                }
+
+                if (output == "out" || output == other)
+                {
+                    continue;
+                }
+
+                result += ProcessNextItem3(output, inputData, endNode, other);
+                // Process each output
+            }
+
+            Memo.Add(node, result);
+            return result;
+        }
+
+        private Dictionary<string, long> Memo = new Dictionary<string, long>();
+
+        public long Part2(string input)
+        {
+            IList<(string id, IList<string> outputs)> inputData = ProcessInput(input);
+
+            var result = 0L;
+
+            Memo.Clear();
+            var svrToFft = ProcessNextItem3("svr", inputData, "fft", "dac");
+
+            Memo.Clear();
+            var svrToDac = ProcessNextItem3("svr", inputData, "dac", "fft");
+
+            Memo.Clear();
+            var DacToFft = ProcessNextItem3("dac", inputData, "fft", "svr");
+
+            Memo.Clear();
+            var FftToDac = ProcessNextItem3("fft", inputData, "dac", "svr");
+
+            Memo.Clear();
+            var fftToOut = ProcessNextItem3("fft", inputData, "out", "dac");
+
+            Memo.Clear();
+            var dacToOut = ProcessNextItem3("dac", inputData, "out", "fft");
+
+            result += svrToDac * DacToFft * fftToOut;
+            result += svrToFft * FftToDac * dacToOut;
+
+            return result;
+        }
+
+        public static IList<(string, IList<string>)> ProcessInput(string input)
+        {
+            var lines = input.Split(new string[] { Environment.NewLine },
+                    StringSplitOptions.None);
+
+            var result = new List<(string, IList<string>)>();
+            foreach (var line in lines)
+            {
+                var parts = line.Split(new[] { ':' }, 2);
+                if (parts.Length == 2)
+                {
+                    var key = parts[0].Trim();
+                    var values = parts[1].Trim().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                        .Select(v => v.Trim()).ToList();
+                    result.Add((key, values));
+                }
+            }
+            return result;
+        }
+    }
+
+}
---
+++ AdventOfCode2025Tests/Day11Test.cs
@@ -0,0 +1,46 @@
+using AdventOfCode2025;
+using AdventOfCode2025Tests.Input;
+
+namespace AdventOfCode2025Tests
+{
+    [TestClass]
+    public class Day11Test
+    {
+        private const long AnswerPart1 = 634;
+        private const long AnswerPart2 = 377452269415704;
+        private const long AnswerExamplePart1 = 5;
+        private const long AnswerExamplePart2 = 2;
+
+
+        private readonly IDay<long> day = new Day11();
+        private readonly Day11Input input = new Day11Input();
+
+        [TestMethod]
+        public void Part1Example()
+        {
+            var result = day.Part1(input.ExampleInput);
+            Assert.AreEqual(AnswerExamplePart1, result);
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            var result = day.Part1(input.Input);
+            Assert.AreEqual(AnswerPart1, result);
+        }
+
+        [TestMethod]
+        public void Part2Example()
+        {
+            var result = day.Part2(input.ExampleInput2);
+            Assert.AreEqual(AnswerExamplePart2, result);
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            var result = day.Part2(input.Input);
+            Assert.AreEqual(AnswerPart2, result);
+        }
+    }
+}
